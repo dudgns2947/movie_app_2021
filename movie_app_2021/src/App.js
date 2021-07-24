946bd8b6613aa53951890e04bb9140ff3c987f26
YeongHun kim <dudgns2947@gmail.com>
2021-07-24 23:39:43 +0900

    import API

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import React from "react";
 import './App.css';
-import PropTypes from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
 
@@ -12,7 +11,8 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json")
+    const { data: { data: { movies } } } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -20,10 +20,19 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
     return <div>
-      {isLoading ? "Loading..." : "We are ready"}
+      {isLoading ? "Loading..." : movies.map(movie => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          year={movie.year}
+          title={movie.title}
+          summary={movie.summary}
+          poster={movie.medium_cover_image} />
+      )
+      )}
     </div>;
   };
 }
